9a2ffc02c1b9baa4fb76ea3775c2957214aa031a
Wilhans Souza <wilhans1984@hotmail.com>
2018-09-27 09:06:59 -0300

    Testes iniciais com movimentos possiveis das pecas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -23,6 +23,14 @@ namespace xadrez_console {
 
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine();
                     Console.Write("destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
--- xadrez-console/tabuleiro/peca.cs
+++ xadrez-console/tabuleiro/peca.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace tabuleiro {
-    class Peca {
+    abstract class Peca {
 
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
@@ -28,5 +28,7 @@ namespace tabuleiro {
         public void incrementarQtdeMovimentos() {
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
     }
 }
