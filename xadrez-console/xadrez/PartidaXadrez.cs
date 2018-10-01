2349434dbfcfda27c4c52b901f5c94f3f965cba0
Wilhans Souza <wilhans1984@hotmail.com>
2018-10-01 08:54:25 -0300

    Jogada especial roque pequeno e roque grande

--- xadrez-console/xadrez/PartidaXadrez.cs
+++ xadrez-console/xadrez/PartidaXadrez.cs
@@ -38,6 +38,24 @@ namespace xadrez {
                 capturadas.Add(pecaCapturada);
             }
 
+            //#jogada especial roque pequeno
+            if(p is Rei && destino.coluna == origem.coluna + 2) {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(origem);
+                T.incrementarQtdeMovimentos();
+                tab.colocarPeca(T, destinoT);
+            }
+
+            //#jogada especial roque grande
+            if (p is Rei && destino.coluna == origem.coluna - 2) {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(origem);
+                T.incrementarQtdeMovimentos();
+                tab.colocarPeca(T, destinoT);
+            }
+
             return pecaCapturada;
 
         }
@@ -96,6 +114,24 @@ namespace xadrez {
                 capturadas.Remove(pecaCaputada);
             }
             tab.colocarPeca(p, origem);
+
+            //#jogada especial roque pequeno
+            if (p is Rei && destino.coluna == origem.coluna + 2) {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(destinoT);
+                T.decrementarQtdeMovimentos();
+                tab.colocarPeca(T, origemT);
+            }
+
+            //#jogada especial roque pequeno
+            if (p is Rei && destino.coluna == origem.coluna - 2) {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(destinoT);
+                T.decrementarQtdeMovimentos();
+                tab.colocarPeca(T, origemT);
+            }
         }
 
         public HashSet<Peca> pecasCapturadas(Cor cor) {
@@ -190,7 +226,7 @@ namespace xadrez {
             colocarNovaPeca('b', 1, new Cavalo(tab, Cor.Branca));
             colocarNovaPeca('c', 1, new Bispo(tab, Cor.Branca));
             colocarNovaPeca('d', 1, new Dama(tab, Cor.Branca));
-            colocarNovaPeca('e', 1, new Rei(tab, Cor.Branca));
+            colocarNovaPeca('e', 1, new Rei(tab, Cor.Branca, this));
             colocarNovaPeca('f', 1, new Bispo(tab, Cor.Branca));
             colocarNovaPeca('g', 1, new Cavalo(tab, Cor.Branca));
             colocarNovaPeca('h', 1, new Torre(tab, Cor.Branca));
@@ -208,7 +244,7 @@ namespace xadrez {
             colocarNovaPeca('b', 8, new Cavalo(tab, Cor.Preta));
             colocarNovaPeca('c', 8, new Bispo(tab, Cor.Preta));
             colocarNovaPeca('d', 8, new Dama(tab, Cor.Preta));
-            colocarNovaPeca('e', 8, new Rei(tab, Cor.Preta));
+            colocarNovaPeca('e', 8, new Rei(tab, Cor.Preta, this));
             colocarNovaPeca('f', 8, new Bispo(tab, Cor.Preta));
             colocarNovaPeca('g', 8, new Cavalo(tab, Cor.Preta));
             colocarNovaPeca('h', 8, new Torre(tab, Cor.Preta));
--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -8,8 +8,10 @@ using tabuleiro;
 namespace xadrez {
     class Rei : Peca {
 
-       public Rei(Tabuleiro tab, Cor cor) : base(tab, cor) {
+        private PartidaXadrez partida;
 
+       public Rei(Tabuleiro tab, Cor cor, PartidaXadrez partida) : base(tab, cor) {
+            this.partida = partida;
         }
 
         public override string ToString() {
@@ -19,6 +21,11 @@ namespace xadrez {
         private bool podeMover(Posicao pos) {
             Peca p = tab.peca(pos);
             return p == null || p.cor != cor;
+        }
+
+        private bool testeTorrePAraRoque(Posicao pos) {
+            Peca p = tab.peca(pos);
+            return p != null && p is Torre && p.cor == cor && p.qteMovimentos == 0;
         }
 
         public override bool[,] movimentosPossiveis() {
@@ -73,6 +80,29 @@ namespace xadrez {
             pos.definirValores(posicao.linha -1 , posicao.coluna - 1);
             if (tab.posicaoValida(pos) && podeMover(pos)) {
                 mat[pos.linha, pos.coluna] = true;
+            }
+
+            //#jogadaespecial roque
+            if(qteMovimentos == 0 && !partida.xeque) {
+                //#josgada especial roque pequeno
+                Posicao posT1 = new Posicao(posicao.linha, posicao.coluna + 3);
+                if (testeTorrePAraRoque(posT1)) {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna + 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna + 2);
+                    if(tab.peca(p1) == null && tab.peca(p2) ==  null) {
+                        mat[pos.linha, pos.coluna + 2] = true;
+                    }
+                }
+                //#josgada especial roque grande
+                Posicao posT2 = new Posicao(posicao.linha, posicao.coluna - 4);
+                if (testeTorrePAraRoque(posT2)) {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.coluna - 3);
+                    if (tab.peca(p1) == null && tab.peca(p2) == null && tab.peca(p3) == null) {
+                        mat[pos.linha, pos.coluna - 2] = true;
+                    }
+                }
             }
 
             return mat;
